9b08c34fe5ae2f23eb97256a74aa7eff2373e7b2
sebandres <sebandres@gmail.com>
2016-10-10 21:31:11 +1100

    Converted to TypeScript 2

--- src/examples/events/UserSaved.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var UserSaved = function UserSaved() {
-	
-};
-
-module.exports = UserSaved;
--- src/examples/webpack.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	entry: "./app.js",
-	output: {
-		filename: "./scripts/bundle.js"
-	},
-	watch: true
-}
